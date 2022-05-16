0979a6d31ab69970393c0fb8368061bf1b386d19
Jesse <jessesouzaall@gmail.com>
2022-05-15 23:17:22 -0300

    fiz somente um método (post) no meu controller, mas ja criei todas as classes e organizei

---
+++ parking-control/src/main/java/com/api/parkingcontrol/ParkingControlApplication.java
@@ -0,0 +1,22 @@
+package com.api.parkingcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ParkingControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlApplication.class, args);
+		
+	
+	}
+	@GetMapping("/")
+	public String olaMundo(){
+		return "<h1>Parking Spot</h1> Olá, Mundo esse é um teste, aqui teremos vagas de estacionamento";
+	}
+
+}
